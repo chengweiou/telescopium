35b75398fb559e151f76c835015ab402f60c5120
chengweiou <46327939+chengweiou@users.noreply.github.com>
2021-07-07 12:21:36 -0700

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,31 @@
+import { createStore } from 'vuex'
+
+export const store = createStore({
+  modules: moduleList(),
+  plugins: pluginList(),
+})
+
+function moduleList() {
+  const list = import.meta.globEager('./module/*.js')
+  const result = {}
+  for (const e in list) {
+    const matched = e.match(/([A-Za-z0-9-_]+)\./i)
+    if (!matched || matched.length <= 1) {
+      return
+    }
+    const name = matched[1]
+    result[name] = list[e].default
+  }
+  return result
+}
+function pluginList() {
+  const list = import.meta.globEager('./plugin/*.js')
+  const orderList = []
+  for (const e in list) {
+    orderList.push({order: list(e).order, v: list(e).default})
+  }
+  orderList.sort((a, b)=>a.order-b.order)
+  let result = []
+  orderList.forEach(e => result.push(e.v))
+  return result
+}
